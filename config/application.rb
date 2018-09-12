b7b348e528fddd39698d9d7f363777d42263b83a
m.taira <taira.masaru@gmail.com>
2018-09-12 18:44:43 +0900

    add toppage

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,15 @@ Bundler.require(*Rails.groups)
 module Okeiko
   class Application < Rails::Application
     # デフォルトテンプレートエンジン
-    config.generators.template_engine = :slim
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.template_engine :slim
+      g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
+      # g.fixture_replacement :factory_girl, dir: "spec/support/factories"
+    end
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
