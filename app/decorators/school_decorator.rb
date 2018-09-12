9304adc0605482fab9e91e16e0582cdf74fa30bc
m.taira <taira.masaru@gmail.com>
2018-09-12 23:34:43 +0900

    add school index

---
+++ app/decorators/school_decorator.rb
@@ -0,0 +1,28 @@
+class SchoolDecorator < ApplicationDecorator
+  delegate_all
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+  def target_age
+    "#{min_age}歳 〜 #{max_age}歳"
+  end
+
+  def price
+    "#{min_price}円 〜 #{max_price}円"
+  end
+
+  def thumb
+    h.image_tag h.asset_path('thumb.jpg'), class: :thumbnail
+  end
+
+  def category_name
+    category.self_and_ancestors.map {|c| c.name }.join(' > ')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'top#index'
 
-  resources :schools, only: :index
+  resources :schools, only: [:index, :show]
 end
